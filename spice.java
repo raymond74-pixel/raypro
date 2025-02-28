ca4a432b2f31e093b9f08e1a9c67599705b26b55
raymond74-pixel <livigharaymond6@gmail.com>
2025-02-28 22:35:37 +0300

    Update spice
    Website Testing and Maintenance.

--- spice.java
+++ spice.java
@@ -1,16 +1,12 @@
-import java.io.*;
-import java.util.Scanner;
-class mySaving {
-public static void main(String[] args){
-System.out.println("Enter Amount: ");
-double result=0.0;
-Scanner nesta= new Scanner (System.in);
-int num= nesta.nextInt();
-if(num>=0){
-result+= num * 30;
-System.out.println("Amount per month:" + result);
-nesta.close();
+<! Doctype html>
+<title>SHOPPING</title>
+
+.fepa{color:purple;
+font-family: fantasy;
+text-size:20px;
 }
-}
-}
+</style>
+</head>
+
+<div class="fepa"><font>SHOPS CENTER</font></div>
 
